fdc86cd4c5a68ad5628555021a6242f66d9b30ca
IBthemute <147502269+IBthemute@users.noreply.github.com>
2023-10-31 14:14:09 +0000

    ArrayList collection

--- src/Library.java
+++ src/Library.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
+
 public class Library {
-    private Book[] collection;
+    ArrayList<Book> collection;
     private int currentSize;
+    private int maxSize;
 
 
     public Library(int maxSize) {
-        collection = new Book[maxSize];
+        collection = new ArrayList<>();
         currentSize = 0;
+        maxSize = maxSize;
     }
 
 
     public void listBooks() {
         for (int i=0; i<currentSize; i++) {
-            Book currentBook = collection[i];
+            Book currentBook = collection.get(i);
             System.out.print(currentBook.title + ", ");
             System.out.print(currentBook.author + ", ");
             System.out.print(currentBook.getPageCount() + ", ");
@@ -19,18 +23,15 @@ public class Library {
         }
     }
 
-    public Book[] addbook(String title, String author, int pageCount, boolean hardback) {
+    public ArrayList<Book> addBook(String title, String author, int pageCount, boolean hardback) {
         Book theBook;
         theBook = new Book(title, author, pageCount, hardback);
 
-        // add to collection
-        for (int i=0; i<= collection.length; i++) {
-            // check if the item in the array is "null" (has not been set)
-            if (collection[i] == null) {
-                collection[i] = theBook;
-                break;
-            }
+        // no changes
+        if (collection.size() >= maxSize) {
+            throw new RuntimeException("Collection is at maximum capacity, cannot add more books.");
         }
+        collection.add(theBook);
 
         return collection;
     }
